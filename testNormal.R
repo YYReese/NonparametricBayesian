cb424d6d175b9af57c5fc0f20e11dd7adaa1d29f
YYReese <s2265990@ed.ac.uk>
2023-01-15 21:00:10 +0000

    15  Jan

--- testNormal.R
+++ testNormal.R
@@ -4,19 +4,20 @@ a <- -5
 b <- 5
 
 # maximum depth of the tree
-maxK <- 5
+maxK <- 7
 
 nParam <- 2^(maxK + 1) - 2
 breaks <- a + (1:(2^maxK-1)) / (2^maxK) * (b-a) 
 # Set the prior knowledge about alpha
 a_prior <- rep(1/2,  nParam)
 
+
 # observations
 n_obs <- 1000
-y_obs <- rnorm(n_obs)
+y_obs <- rnorm(n_obs) 
 
-# Draw n realisations from posterior PT 
-n <- 1000
+# Draw n realisations from posterior PT and prior PT
+n <- 10000
 prob <- matrix(nrow=n,ncol=nParam)
 for (i in 1:n){
   prob[i,] <- PT_update(a,b,maxK,a_prior, y_obs)
@@ -38,20 +39,24 @@ up_p <- up_p[(length(up_p)-2^(maxK)+1): length(up_p)]
 lw_p <- lw_p[(length(lw_p)-2^(maxK)+1): length(lw_p)]
 
 # Plot mean of the n realizations from PT
-plot(xPoints, p/((b-a)/2^maxK), col=2, type="s", 
+plot(xPoints, p/((b-a)/2^maxK), col=2, type="l", 
+     xlab="y",
      ylim=c(-0.7, 0.7), ylab="Density")
 
-# Plot a sample from prior PT
-lines(xPoints, Cal_prob(draw_theta(a_prior), maxK)
-     [(length(mu_p)-2^(maxK)+1): length(mu_p)],type = "s")
-
 # 90% Confidence interval
-lines(xPoints, up_p/((b-a)/2^maxK),col="grey")
-lines(xPoints, lw_p/((b-a)/2^maxK),col="grey")
+lines(xPoints, up_p/((b-a)/2^maxK),col="grey",type="l")
+lines(xPoints, lw_p/((b-a)/2^maxK),col="grey",type="l")
 
-# Show the true data distribution 
-lines(density(y_obs), col=3, type="l")
+# Show the observation points 
+points(density(y_obs), col=4,pch=3, cex=0.05)
+
+# Standard Gaussian
+lines(xPoints,dnorm(xPoints))
+
+
+title("n=1000 observations",cex=0.3)
 
 legend("topright", pch=c(15,15), 
-       legend=c("PT","True density","90% CI"), 
-       col=c(2,3,"grey"), bty="n", cex=0.5)
+       legend=c("Prior PT", "Post PT","Observations","90% CI"), 
+       col=c("black",2,4,"grey"), bty="n", cex=0.5)
+
